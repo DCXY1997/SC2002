bc35f056259c8894f14dc77cbda80604246a3e51
WeijunCheah <WCHEAH006@e.ntu.edu.sg>
2024-11-16 16:25:17 +0800

    Fixed inventory part

--- src/View/AdminView.java
+++ src/View/AdminView.java
@@ -1,3 +1,4 @@
+
 package src.View;
 
 import src.Controller.AdminController;
@@ -5,27 +6,25 @@ import src.Controller.StaffController;
 import src.Helper.Helper;
 import src.Model.Admin;
 import src.Model.Staff;
-
 import src.Repository.Repository;
 
-public class AdminView extends MainView {
-
-    /**
-     * initialize objects to call their view app
-     */
-    private DisplayStaffView displayStaffView = new DisplayStaffView();
+public class AdminView extends MainView{
+	
+	/**
+	 * initialize objects to call their view app
+	 */
+	private DisplayStaffView displayStaffView = new DisplayStaffView();
     private ManageStaffAccountView manageStaffAccountView = new ManageStaffAccountView();
-    private DisplayAppointmentDetailView displayAppointmentDetailsView = new DisplayAppointmentDetailView();
-    private DisplayMedicationInventory displayMedicalInventoryView = new DisplayMedicationInventory();
-    private ManageMedicalInventory manageMedicalInventory = new ManageMedicalInventory();
-    private ManageReplenishmentRequestView manageReplenishmentRequestView = new ManageReplenishmentRequestView();
-
-    /**
+	private DisplayAppointmentDetailView displayAppointmentDetailsView = new DisplayAppointmentDetailView();
+	private DisplayMedicationInventory displayMedicalInventoryView = new DisplayMedicationInventory();
+	private ManageMedicalInventory manageMedicalInventory = new ManageMedicalInventory();
+	private ManageReplenishmentRequestView manageReplenishmentRequestView = new ManageReplenishmentRequestView();
+	/**
      * View Actions of the AdminView.
      */
     @Override
-    public void printActions() {
-        Helper.clearScreen();
+	public void printActions() {
+		Helper.clearScreen();
         printBreadCrumbs("Hospital Management App View > Login View > Admin View");
         System.out.println("What would you like to do ?");
         System.out.println("(1) View Hospital Staff");
@@ -34,16 +33,16 @@ public class AdminView extends MainView {
         System.out.println("(4) View Medication Inventory");
         System.out.println("(5) Add/Remove/Update Medication Inventory");
         System.out.println("(6) Approve Replenishment Requests");
-        System.out.println("(7) Change Password");
+		System.out.println("(7) Change Password");
         System.out.println("(8) Logout");
-    }
+	}
 
     @Override
-    public void viewApp() {
-        int opt = -1;
-        do {
+	public void viewApp() { 
+		int opt = -1; 
+		do { 
             printActions();
-            opt = Helper.readInt(1, 8);
+            opt = Helper.readInt(1,8);
             switch (opt) {
                 case 1:
                     Helper.clearScreen();
@@ -58,23 +57,23 @@ public class AdminView extends MainView {
                     displayAppointmentDetailsView.viewApp();
                     break;
                 case 4:
-                    Helper.clearScreen();
-                    displayMedicalInventoryView.viewApp();
+                	Helper.clearScreen();
+                	displayMedicalInventoryView.viewApp();
                     break;
                 case 5:
-                    Helper.clearScreen();
-                    manageMedicalInventory.viewApp();
-                    break;
+					Helper.clearScreen();
+					manageMedicalInventory.viewApp();
+					break;
                 case 6:
-                    Helper.clearScreen();
-                    manageReplenishmentRequestView.viewApp();
-                    break;
+                	Helper.clearScreen();
+                	manageReplenishmentRequestView.viewApp();
+                	break;
                 case 7:
-                    printBreadCrumbs("Hospital Management App View > Login View > Admin View > Password Change View");
-                    promptChangePassword();
-                    break;
+                	printBreadCrumbs("Hospital Management App View > Login View > Admin View > Password Change View");
+                	promptChangePassword();
+                	break;
                 case 8:
-                    break;
+					break;
                 default:
                     System.out.println("Invalid option");
                     break;
@@ -83,33 +82,34 @@ public class AdminView extends MainView {
                 Helper.pressAnyKeyToContinue();
             }
         } while (opt != 8);
-    }
+	}
 
-    private void promptChangePassword() {
-        System.out.println("Verify your loginID: ");
-        String loginId = Helper.readString();
-        System.out.println("Verify your password: ");
-        String password = Helper.readString();
-
-        Staff staff = Repository.STAFF.get(loginId);
-
-        // Check if the staff exists and verify the password
-        if (staff != null && staff.getPassword().equals(password)) {
-            System.out.println("Verification successful");
-
-            System.out.println("Enter new password: ");
-            String newPassword = Helper.readString();
-            System.out.println("Re-enter new password: ");
-            String confirmPassword = Helper.readString();
-
-            if (StaffController.changePassword(staff, newPassword, confirmPassword)) {
-                System.out.println("Password changed successfully!");
-            } else {
-                System.out.println("Passwords do not match.");
-            }
-        } else {
-            System.out.println("Verification failed. Either the user ID or password is incorrect.");
-        }
-    }
-
+	private void promptChangePassword() {
+		System.out.println("Verify your loginID: ");
+		String loginId = Helper.readString();
+		System.out.println("Verify your password: ");
+		String password = Helper.readString();
+	
+		Staff staff = Repository.STAFF.get(loginId);
+	
+		// Check if the staff exists and verify the password
+		if (staff != null && staff.getPassword().equals(password)) {
+			System.out.println("Verification successful");
+	
+			System.out.println("Enter new password: ");
+			String newPassword = Helper.readString();
+			System.out.println("Re-enter new password: ");
+			String confirmPassword = Helper.readString();
+	
+			if (StaffController.changePassword(staff, newPassword, confirmPassword)) {
+				System.out.println("Password changed successfully!");
+			} else {
+				System.out.println("Passwords do not match.");
+			}
+		} else {
+			System.out.println("Verification failed. Either the user ID or password is incorrect.");
+		}
+	}
+	
+    
 }
--- src/View/ManageMedicalInventory.java
+++ src/View/ManageMedicalInventory.java
@@ -1,15 +1,16 @@
 package src.View;
 
+import src.Controller.AdminController;
 import src.Controller.InventoryController;
-import src.Enum.MedicineStatus;
+import src.Enum.Gender;
+import src.Enum.StaffType;
 import src.Helper.Helper;
 import src.Repository.Repository;
 
-public class ManageMedicalInventory extends MainView {
-
-    @Override
-    public void printActions() {
-        Helper.clearScreen();
+public class ManageMedicalInventory extends MainView{
+     @Override
+	public void printActions() {
+		Helper.clearScreen();
         printBreadCrumbs("Hospital Management App View > Login View > Admin View > Manage Staff Account View");
         System.out.println("What would you like to do ?");
         System.out.println("(1) Add Medical Inventory Item");
@@ -19,7 +20,7 @@ public class ManageMedicalInventory extends MainView {
     }
 
     @Override
-    public void viewApp() {
+	public void viewApp() {
         int opt = -1;
         do {
             printActions();
@@ -27,24 +28,21 @@ public class ManageMedicalInventory extends MainView {
             switch (opt) {
                 case 1:
                     Helper.clearScreen();
-                    printBreadCrumbs(
-                            "Hospital Management App View > Login View > Admin View > Manage Medical Inventory View > Add Medical InventoryView");
+                    printBreadCrumbs("Hospital Management App View > Login View > Admin View > Manage Medical Inventory View > Add Medical InventoryView");
                     promptAddMedicalInventory();
                     break;
-                case 2:
+                case 2: 
                     Helper.clearScreen();
-                    printBreadCrumbs(
-                            "Hospital Management App View > Login View > Admin View > Manage Medical Inventory View > Remove Medical Inventory View");
+                    printBreadCrumbs("Hospital Management App View > Login View > Admin View > Manage Medical Inventory View > Remove Medical Inventory View");
                     promptRemoveMedicalInventory();
                     break;
                 case 3:
                     Helper.clearScreen();
-                    printBreadCrumbs(
-                            "Hospital Management App View > Login View > Admin View > Manage Medical Inventory View > Update Medical Inventory View");
+                    printBreadCrumbs("Hospital Management App View > Login View > Admin View > Manage Medical Inventory View > Update Medical Inventory View");
                     promptUpdateMedicalInventory();
                     break;
                 case 4:
-                    break;
+                	break;
                 default:
                     System.out.println("Invalid option");
                     break;
@@ -53,50 +51,18 @@ public class ManageMedicalInventory extends MainView {
                 Helper.pressAnyKeyToContinue();
             }
         } while (opt != 4);
-    }
+	}
+
 
     private boolean promptAddMedicalInventory() {
         System.out.println("Enter Medicine Name:");
         String name = Helper.readString();
-
+    
         System.out.println("Enter Medicine Price:");
         float price = (float) Helper.readFloat();
 
         System.out.println("Enter Medicine Description:");
         String description = Helper.readString();
-
-        // --------------------------------------------------
-        // Medicine Status CHG HERE
-        // Prompt the user to enter a valid medicine status
-        System.out.println("Enter Medicine Status (PENDING/DISPENSED):");
-
-        // Read the user input
-        String statusInput = Helper.readString().toUpperCase().trim(); // Convert to uppercase and trim whitespace
-
-        // Use a switch statement to handle the two valid status options
-        MedicineStatus status;
-        switch (statusInput) {
-            case "PENDING":
-                status = MedicineStatus.PENDING;
-                // Perform actions for PENDING status if needed
-                break;
-            case "DISPENSED":
-                status = MedicineStatus.DISPENSED;
-                // Perform actions for DISPENSED status if needed
-                break;
-            default:
-                // Handle invalid input
-                System.out.println("Invalid medicine status entered. Please try again.");
-                // Optionally, you could loop or prompt for the status again
-                status = null; // Set to null or handle appropriately
-                break;
-        }
-
-        // Use the status variable if it is not null
-        if (status != null) {
-            // Continue with your code using the valid status
-        }
-        // --------------------------------------------------
 
         System.out.println("Enter Medicine Initial Stock Count:");
         int stock = Helper.readInt();
@@ -105,80 +71,81 @@ public class ManageMedicalInventory extends MainView {
         int stockLow = Helper.readInt();
 
         String medicineId = generateMedicalInventoryId(name);
-
-        // ADD STATUS
-        InventoryController.addMedicalInventory(medicineId, name, status, price, description, stock, stockLow);
+    
+        InventoryController.addMedicalInventory(medicineId, name, price, description, stock, stockLow);
         return true;
     }
-
+    
     public String generateMedicalInventoryId(String name) {
         String prefix = "";
         int uniqueId = Helper.generateUniqueId(Repository.INVENTORY);
-        return prefix + String.format("%03d", uniqueId);
+        return prefix + String.format("%03d", uniqueId); 
     }
 
     public boolean promptRemoveMedicalInventory() {
-        System.out.println("Enter the medical inventory item id you want to remove: ");
-        String medicineId = Helper.readString();
+    System.out.println("Enter the medical inventory item id you want to remove: ");
+    String medicineId = Helper.readString();
+    
+    if (Repository.INVENTORY.containsKey(medicineId)) {
+        if (!InventoryController.removeMedicalInventoryItem(medicineId)) {
+            System.out.println("Medical Inventory Removal canceled!");
+            return false;
+        }
+    } else {
+        System.out.println("Medical Inventory Item Not Found!");
+        return false;
+    }
+    return true;
+}
 
-        if (Repository.INVENTORY.containsKey(medicineId)) {
-            if (!InventoryController.removeMedicalInventoryItem(medicineId)) {
-                System.out.println("Medical Inventory Removal canceled!");
-                return false;
-            }
+private boolean promptUpdateMedicalInventory() {
+    Helper.clearScreen();
+    printBreadCrumbs("Hospital Management View > Admin View > Update a Medical Inventory Item");
+
+    System.out.println("Enter the medical inventory ID that you want to update: ");
+    String medicineId = Helper.readString();
+
+    // Check if the medicineId exists in INVENTORY via the controller
+    if (InventoryController.searchMedicalInventoryById(medicineId) == null) {
+        System.out.println("Medical inventory item not found!");
+        return false;
+    }
+
+    System.out.println("What would you like to update?");
+    System.out.println("(1) Update Stock Count");
+    System.out.println("(2) Update Low Stock Level Alert");
+    int choice = Helper.readInt(1, 2);
+
+    boolean success = false;
+    
+    if (choice == 1) {
+        System.out.println("Please enter the new stock level: ");
+        int newStockLevel = Helper.readInt();
+        success = InventoryController.updateMedicalInventoryStockLevel(medicineId, newStockLevel);
+        
+        // Print success or failure message
+        if (success) {
+            System.out.println("Stock level updated successfully for Medicine ID: " + medicineId);
         } else {
-            System.out.println("Medical Inventory Item Not Found!");
-            return false;
+            System.out.println("Failed to update stock level for Medicine ID: " + medicineId);
         }
-        return true;
+
+    } else if (choice == 2) {
+        System.out.println("Please enter the new low stock level alert: ");
+        int newLowStockLevelAlert = Helper.readInt();
+        success = InventoryController.updateMedicalInventoryLowStockAlert(medicineId, newLowStockLevelAlert);
+        
+        // Print success or failure message
+        if (success) {
+            System.out.println("Low stock level alert updated successfully for Medicine ID: " + medicineId);
+        } else {
+            System.out.println("Failed to update low stock level alert for Medicine ID: " + medicineId);
+        }
     }
 
-    private boolean promptUpdateMedicalInventory() {
-        Helper.clearScreen();
-        printBreadCrumbs("Hospital Management View > Admin View > Update a Medical Inventory Item");
+    return success;
+}
 
-        System.out.println("Enter the medical inventory ID that you want to update: ");
-        String medicineId = Helper.readString();
 
-        // Check if the medicineId exists in INVENTORY via the controller
-        if (InventoryController.searchMedicalInventoryById(medicineId) == null) {
-            System.out.println("Medical inventory item not found!");
-            return false;
-        }
-
-        System.out.println("What would you like to update?");
-        System.out.println("(1) Update Stock Count");
-        System.out.println("(2) Update Low Stock Level Alert");
-        int choice = Helper.readInt(1, 2);
-
-        boolean success = false;
-
-        if (choice == 1) {
-            System.out.println("Please enter the new stock level: ");
-            int newStockLevel = Helper.readInt();
-            success = InventoryController.updateMedicalInventoryStockLevel(medicineId, newStockLevel);
-
-            // Print success or failure message
-            if (success) {
-                System.out.println("Stock level updated successfully for Medicine ID: " + medicineId);
-            } else {
-                System.out.println("Failed to update stock level for Medicine ID: " + medicineId);
-            }
-
-        } else if (choice == 2) {
-            System.out.println("Please enter the new low stock level alert: ");
-            int newLowStockLevelAlert = Helper.readInt();
-            success = InventoryController.updateMedicalInventoryLowStockAlert(medicineId, newLowStockLevelAlert);
-
-            // Print success or failure message
-            if (success) {
-                System.out.println("Low stock level alert updated successfully for Medicine ID: " + medicineId);
-            } else {
-                System.out.println("Failed to update low stock level alert for Medicine ID: " + medicineId);
-            }
-        }
-
-        return success;
-    }
 
 }
